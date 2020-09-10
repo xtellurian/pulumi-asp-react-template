2a6d57656f0a81eaf4a436b629ede6b8d8cb2753
Rian Finnegan <rian@amphoradata.com>
2020-09-10 14:27:55 +1000

    fix: don't login that way

--- src/deploy.sh
+++ src/deploy.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
-pushd infra
 ACR_NAME=$1
-popd
 pushd app
-az acr login -n $ACR_NAME
 docker build -t $ACR_NAME.azurecr.io/app:latest .
 docker push $ACR_NAME.azurecr.io/app:latest
